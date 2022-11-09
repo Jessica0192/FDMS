3f17940d30b2dc8754a1e16db05b0827b43cfa5d
TroyAlexanderHill <Thill7222@conestogac.on.ca>
2022-11-09 03:52:49 -0500

    Implemented fully functioning database logic, this includes new database schema alongside it

---
+++ Ground Terminal/Logger.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ground_Terminal
+{
+    internal class Logger
+    {
+        /// <summary>
+        /// Log to eventlog and textfile
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Log(string message)
+        {
+            //declare date variable
+            DateTime now = DateTime.Now;
+            //set file path
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + @".\GroundTerminalLogger.txt";
+            string fullmessage = "";
+            //create message to log
+            fullmessage = message + "-->" + now.ToString("F") + "\n";
+            //check if file exists
+            if (!File.Exists(filePath))
+            {
+                StreamWriter createFile = File.CreateText(filePath);
+                createFile.Close();
+            }
+
+            //instantiate event log
+            EventLog serviceEventLog = new EventLog();
+            //check if the event log already exists
+            if (!EventLog.SourceExists("MyEventSource"))
+            {
+                EventLog.CreateEventSource("MyEventSource", "MyEventLog");
+            }
+            //set source for log
+            serviceEventLog.Source = "MyEventSource";
+            //set the name of the event log
+            serviceEventLog.Log = "MyEventLog";
+            //write message to log event
+            serviceEventLog.WriteEntry(fullmessage);
+            //write message to text file for logs
+            StreamWriter sw = File.AppendText(filePath);
+            sw.Write(fullmessage);
+            //close file stream
+            sw.Close();
+        }
+    }
+}
--- Ground Terminal/MainWindow.xaml.cs
+++ Ground Terminal/MainWindow.xaml.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Messaging;
 using System.Windows;
+using System.Windows.Media.Media3D;
 
 namespace Ground_Terminal
 {
@@ -13,8 +19,24 @@ namespace Ground_Terminal
         public MainWindow()
         {
             InitializeComponent();
+            List<AircraftTelemetryData> data = new List<AircraftTelemetryData>();
+           
+            data.Add(new AircraftTelemetryData()
+            {
+                Timestamp = DateTime.Now,
+                AccelX = 2.23,
+                AccelY = 4.45,
+                AccelZ = 3.14,
+                Weight = 10.23,
+                Altitude = 573.85,
+                Pitch = 34.4,
+                Bank = 23.3
+        });
+            WriteCollectionData(data);
             LoadDataToGrid();
         }
+
+        public bool isRealTime = false;
 
         private void LoadDataToGrid()
         {
@@ -26,58 +48,154 @@ namespace Ground_Terminal
             MessageBox.Show("On every key press");
         }
 
+        //private List<AircraftTelemetryData> recieveTelData()
+        //{
+        //    try
+        //    {
+        //        List<AircraftTelemetryData> data = new List<AircraftTelemetryData>();
+        //        Int32 port = 13000;
+        //        TcpClient tcpClient = new TcpClient("127.0.0.1", port);
+        //        string[] dataPacket = System.Text.Encoding.ASCII.GetString();
+
+        //        if(isRealTime ==  false)
+        //        {
+
+        //        }
+        //        else if (isRealTime ==  true)
+        //        {
+
+        //        }
+        //    }
+        //    catch(SocketException e)
+        //    {
+
+        //    }
+        //}
+
+        private void WriteCollectionData(List<AircraftTelemetryData> telData)
+        {
+            String connectionString = @"server=localhost;database=FDMS_Test;trusted_connection=true";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+
+                string dataWrite = "INSERT INTO Telemetry_Test (Tail_Number, Date_Time_Stamp, AccelX, AccelY, AccelZ, telWeight, Altitude, Pitch, Bank)";
+                dataWrite += " VALUES (@telTailNum, @telDate , @telAccelX, @telAccelY, @telAccelZ, @telWeightPar, @telAlt, @telPitch, @telBank)";
+
+                SqlCommand command = new SqlCommand(dataWrite, connection);
+                //command.Parameters.AddWithValue("@telNum", 1);
+                command.Parameters.AddWithValue("@telTailNum", 3456);
+                command.Parameters.AddWithValue("@telDate", telData[0].Timestamp);
+                command.Parameters.AddWithValue("@telAccelX", telData[0].AccelX);
+                command.Parameters.AddWithValue("@telAccelY", telData[0].AccelY);
+                command.Parameters.AddWithValue("@telAccelZ", telData[0].AccelZ);
+                command.Parameters.AddWithValue("@telWeightPar", telData[0].Weight);
+                command.Parameters.AddWithValue("@telAlt", telData[0].Altitude);
+                command.Parameters.AddWithValue("@telPitch", telData[0].Pitch);
+                command.Parameters.AddWithValue("@telBank", telData[0].Bank);
+
+               // command.Prepare();
+               int result = command.ExecuteNonQuery();
+                if(result == 1)
+                {
+                    Console.WriteLine("Success");
+                }
+                connection.Close();
+            }
+            catch(SqlException e)
+            {
+                throw e;
+            }
+        }
+
         private List<AircraftTelemetryData> LoadCollectionData()
         {
-            String connectionString = "";
+            String connectionString = @"server=localhost; database=FDMS_Test;trusted_connection=true";
             SqlConnection connection = new SqlConnection(connectionString);
-            SqlDataReader reader = null;
             List<AircraftTelemetryData> data = new List<AircraftTelemetryData>();
+            SqlCommand readTelcmd = new SqlCommand("SELECT TOP 15 Tail_Number, Date_Time_Stamp, AccelX, AccelY, AccelZ, telWeight, Altitude, Pitch, Bank FROM Telemetry_Test", connection);
+            //SqlCommand readGForcecmd = new SqlCommand("SELECT TOP 15 FROM GForce", connection);
+            //SqlCommand readAltcmd = new SqlCommand("SELECT TOP 15 FROM Altitude", connection);
 
-            //connection.Open();
-            //while (reader.Read())
-            //{
-            //    data.Add(new AircraftTelemetryData()
-            //    {
-            //        Timestamp = reader.GetDateTime(0),
-            //        AccelX = reader.GetDouble(1),
-            //        AccelY = reader.GetDouble(2),
-            //        AccelZ = reader.GetDouble(3),
-            //        Weight = reader.GetDouble(4),
-            //        Altitude = reader.GetDouble(5),
-            //        Pitch = reader.GetDouble(6),
-            //        Bank = reader.GetDouble(7)
-            //    });
-            //}
-            //use loop to generate list of AircraftTelemetryData object with data from DB
-            for(int i=0; i<15; i++)
+            connection.Open();
+
+            SqlDataReader reader = readTelcmd.ExecuteReader();
+
+            while (reader.Read())
             {
                 data.Add(new AircraftTelemetryData()
                 {
-                    Timestamp = new DateTime(),
-                    AccelX = Convert.ToDouble(i),
-                    AccelY = Convert.ToDouble(i),
-                    AccelZ = Convert.ToDouble(i),
-                    Weight = Convert.ToDouble(i),
-                    Altitude = Convert.ToDouble(i),
-                    Pitch = Convert.ToDouble(i),
-                    Bank = Convert.ToDouble(i)
+                    Timestamp = reader.GetDateTime(1),
+                    AccelX = reader.GetDouble(2),
+                    AccelY = reader.GetDouble(3),
+                    AccelZ = reader.GetDouble(4),
+                    Weight = reader.GetDouble(5),
+                    Altitude = reader.GetDouble(6),
+                    Pitch = reader.GetDouble(7),
+                    Bank = reader.GetDouble(8)
                 });
             }
+            reader.Close();
+            connection.Close();
+
+           // connection.Open();
+
+            //reader = readGForcecmd.ExecuteReader();
+            //while(reader.Read())
+            //{
+            //    data.Add(new AircraftTelemetryData()
+            //    {
+            //        AccelX = reader.GetDouble(1),
+            //        AccelY = reader.GetDouble(2),
+            //        AccelZ = reader.GetDouble(3),
+            //        Weight = reader.GetDouble(4)
+            //    });
+            //}
+            //reader.Close();
+            //connection.Close();
+
+            
+            //use loop to generate list of AircraftTelemetryData object with data from DB
+            //for (int i=0; i<15; i++)
+            //{
+            //    data.Add(new AircraftTelemetryData()
+            //    {
+            //        Timestamp = new DateTime(),
+            //        AccelX = Convert.ToDouble(i),
+            //        AccelY = Convert.ToDouble(i),
+            //        AccelZ = Convert.ToDouble(i),
+            //        Weight = Convert.ToDouble(i),
+            //        Altitude = Convert.ToDouble(i),
+            //        Pitch = Convert.ToDouble(i),
+            //        Bank = Convert.ToDouble(i)
+            //    });
+            //}
             
 
             return data;
         }
 
+        //private string CalculateCheckSum(int checksum)
+        //{
+        //    string result = checksum.ToString();
+        //    return result;
+        //}
+
         private void toggleRealTimeMode_Checked(object sender, RoutedEventArgs e)
         {
+            isRealTime = true;
             MessageBox.Show("Button toggled on!");
         }
 
         private void toggleRealTimeMode_Unchecked(object sender, RoutedEventArgs e)
         {
+            isRealTime = false;
             MessageBox.Show("Button toggled off!");
         }
     }
+
 
     public class AircraftTelemetryData
     {
